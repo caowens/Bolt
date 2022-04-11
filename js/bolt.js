1f35ed15aa77d82b66cdadfd0b907f9d150de9b8
caowens <70489592+caowens@users.noreply.github.com>
2022-04-11 14:33:31 -0500

    started functionality for js

--- js/bolt.js
+++ js/bolt.js
@@ -1,104 +1,32 @@
-let users = [
-    {
-        username:"charles123",
-        email:"charles123@gmail.com",
-        password:"charles_pass"
-    },
-    {
-        username:"asha123", 
-        email:"asha123@gmail.com",
-        password:"asha_pass"
-    },
-    {
-        username:"timia123",
-        email:"timia123@gmail.com",
-        password:"timia_pass"
-    },
-    {
-        username:"feyi123",
-        email:"feyi123@gmail.com",
-        password:"feyi_pass"
-    },
-    {
-        username:"justin123",
-        email:"justin123@gmail.com",
-        password:"justin_pass"
-    }
-]
+const APIPath = "../JSON:test APIs/users.json";
+const numberOfResults = 9;
+const workGrid = document.querySelector('#please-work');
+const searchBtn = document.querySelector('#btn-search');
+let cartoons = [];
 
-let groups = [
-    {
-        host:"charles123",
-        name:"Corvette Corvette",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo similique amet atque reprehenderit sequi nostrum fuga, accusamus molestias natus, quae eos possimus, iure praesentium nobis laboriosam provident itaque laborum.",
-        location:"San Francisco, CA",
-        image:""
-    },
-    {
-        host:"timia123",
-        name:"Mighty Mustangs",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo similique amet atque reprehenderit sequi nostrum fuga, accusamus molestias natus, quae eos possimus, iure praesentium nobis laboriosam provident itaque laborum.",
-        location:"Seattle, WA",
-        image:""
-    },
-    {
-        host:"asha123",
-        name:"Tesla Life",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo similique amet atque reprehenderit sequi nostrum fuga, accusamus molestias natus, quae eos possimus, iure praesentium nobis laboriosam provident itaque laborum.",
-        location:"Austin, TX",
-        image:""
-    },
-    {
-        host:"feyi123",
-        name:"Camara Joyriders",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo similique amet atque reprehenderit sequi nostrum fuga, accusamus molestias natus, quae eos possimus, iure praesentium nobis laboriosam provident itaque laborum.",
-        location:"Nashville, TN",
-        image:""
-    },
-    {
-        host:"justin123",
-        name:"GTR",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo similique amet atque reprehenderit sequi nostrum fuga, accusamus molestias natus, quae eos possimus, iure praesentium nobis laboriosam provident itaque laborum.",
-        location:"NYC, NY",
-        image:""
-    }
-]
+fetch("../JSON:test APIs/users.json")
+    .then(response => response.json())
+    .then((data) => {
+        users = data;
+        logUsers(data);
+    });
 
-let events = [
-    {
-        name:"Corvette Spring Fling",
-        date:"04/12/22",
-        address:"123 Fast Track Lane",
-        group:"Corvette Corvette",
-        image:""
-    },
-    {
-        name:"Mustangs Summer Blowout",
-        date:"05/23/22",
-        address:"456 Faster Than You Blvd",
-        group:"Mighty Mustangs",
-        image:""
-    },
-    {
-        name:"Elon Twitter Celebration",
-        date:"04/20/22",
-        address:"1 Tesla Road",
-        group:"Tesla Life",
-        image:""
-    },
-    {
-        name:"Jumping Joyride",
-        date:"06/21/22",
-        address:"789 Racetrack Dr",
-        group:"Camara Joyriders",
-        image:""
-    },
-    {
-        name:"Gatorade Refuel",
-        date:"05/18/22",
-        address:"147 Just Do It Ct",
-        group:"gtr",
-        image:""
-    }
-]
-
+function logUsers(users) {
+    let allCardsDom = '';
+    users.forEach((user)=>{
+        const cardTemplate = 
+            `<div class="col">
+                <div class="card">
+                    <img src="../images/bolt-background.png"
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${user.username}</h5>
+                        <p class="card-text">${user.email}</p>
+                    </div>
+                </div>
+            </div>`; 
+        console.log(user.email);
+        allCardsDom +=cardTemplate;
+    })
+    workGrid.innerHTML = allCardsDom;
+}
