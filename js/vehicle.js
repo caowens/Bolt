554f18de8cb35a401b42e1f6b18ddeba2fdc3e2d
FeyiAdeaga <feyithereal@gmail.com>
2022-04-12 00:04:08 -0500

    Changes to vehicle page design

--- js/vehicle.js
+++ js/vehicle.js
@@ -1,3 +1,6 @@
+const carddata = document.querySelector("#data");
+cars = []
+
 const getVehicle = async() =>{
     const response = await fetch("../JSON:test APIs/vehicle.json");
     // For debugging server error
@@ -8,30 +11,61 @@ const getVehicle = async() =>{
     return data
 }
 // console.log(1);
+
+updateCars = (data) =>{
+  const cardata = data;
+  
+  replacer = ""
+
+  cardata.forEach(car=>{
+      text = `<div class="col">
+      <div class="card">
+        <img src="${car.Img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <div class="car-make"><h5 class="card-title">${car.Make}</h5><h7 class="card-title">${car.Model}</h7></div>
+          <p class="card-text"><ul>
+          <li>0-60: ${car["0-60"]}s</li>
+          <li>Quarter Mile: ${car["Quarter Mile"]}s</li>
+          </ul></p>
+        </div>
+      </div>
+    </div>`
+
+    replacer+=text;
+  })
+
+
+  carddata.innerHTML = replacer;
+}
 getVehicle()
     .then(data=>{
-       const cardata = data;
-       const carddata = document.querySelector("#data");
-    
-       replacer = ""
-
-       cardata.forEach(car=>{
-           text = `<div class="col">
-           <div class="card">
-             <img src="${car.Img}" class="card-img-top" alt="...">
-             <div class="card-body">
-               <div class="car-make"><h5 class="card-title">${car.Make}</h5><h7 class="card-title">${car.Model}</h7></div>
-               <p class="card-text"><ul>
-               <li>0-60: ${car["0-60"]}s</li>
-               <li>Quarter Mile: ${car["Quarter Mile"]}s</li>
-               </ul></p>
-             </div>
-           </div>
-         </div>`
-
-         replacer+=text;
-       })
+      cars = data;
+      updateCars(data);
+})
 
 
-       carddata.innerHTML = replacer;
-    })
+
+const searchBtn = document.querySelector('#btn-search');
+
+function filterByName() {
+  const searchInput = document.querySelector('#search-input').value;
+  let filteredArray = [];
+  if(searchInput && searchInput.length){
+      cars.forEach((car) => {
+          if(car.Make.toLowerCase()
+              .startsWith(searchInput.toLowerCase()))
+              filteredArray.push(car);
+      });
+  }else {
+      return cars;
+      console.log("ran")
+  }
+  console.log({searchInput});
+  return filteredArray;
+}
+
+searchBtn.addEventListener('click',(e)=>{
+  console.log('Search button clicked...');
+  let filteredList = filterByName();
+  updateCars(filteredList);
+});
