e218b26a48ae1aa5d48c115d05aefc7367194110
caowens <70489592+caowens@users.noreply.github.com>
2022-04-12 15:13:35 -0500

    functionality for saving a form to json

--- js/create-group.js
+++ js/create-group.js
@@ -1,7 +1,3 @@
-// var image = document.getElementById("group-image").value;
-// var name = document.getElementById("group-name").value;
-// var description = document.getElementById("group-purpose").value;
-
 function handleSubmit(event) {
     event.preventDefault();
   
